040c646eb9904a128e749b509d5773af6fc4b31d
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-06-14 11:18:54 +0600

    hash password and login system

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const uri = process.env.DB_URI;
 mongoose
@@ -15,9 +17,11 @@ const productSchema = new mongoose.Schema(
     price: String,
     color: String,
     sell: Number,
+    password: String,
+    email: String,
   },
   {
-    timestamps: true, 
+    timestamps: true,
   }
 );
 const Product = mongoose.model("Product", productSchema);
@@ -35,9 +39,43 @@ app.get("/", (req, res) => {
 
 app.post("/iphone", async (req, res) => {
   try {
-    const phoneData = new Product(req.body);
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    const password = hash;
+    const dataObject = {
+      model: req.body.model,
+      price: req.body.price,
+      color: req.body.color,
+      sell: req.body.sell,
+      email: req.body.email,
+      password: password,
+    };
+    const phoneData = new Product(dataObject);
     await phoneData.save();
     res.status(201).json(phoneData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+//! Login a user using email and password
+
+app.post("/iphone/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const phoneData = await Product.findOne({ email: email });
+    if (!phoneData) {
+      res.status(404).json({ message: "data not found" });
+    } else {
+      const validityCheck = await bcrypt.compare(password, phoneData.password);
+      if(!validityCheck){
+      res.status(401).json({ message: "wrong password" });
+
+      }else{
+        res.status(201).json(phoneData);
+      }
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "something went wrong" });
@@ -73,6 +111,7 @@ app.put("/iphone/:id", async (req, res) => {
       phoneData.model = req.body.model;
       phoneData.price = req.body.price;
       phoneData.color = req.body.color;
+      phoneData.sell = req.body.sell;
 
       res.json(phoneData);
     } else {
